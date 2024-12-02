7fcbd0c699040041affc94bb54774701482c29d1
jdbrwnjr <jdbrwnjr@gmail.com>
2024-12-02 16:02:32 +0000

    Ch.11 HW

--- hw11-21.cpp
+++ hw11-21.cpp
@@ -6,14 +6,19 @@
 using namespace std;
 
 int main()
+
 {
-	double prices[10] = {96.5, 100.5, 100.5, 100.5, 99, 99, 99, 100, 98.5, 98.9};
-	double total = 0.0;
-	double average = 0.0;
+		 double prices[10] = {96.5, 100.5, 100.5, 100.5, 99, 99, 99, 100, 98.5, 98.9};
+		 double total = 0.0;
+		 double average = 0.0;
 
+		// total sum of prices 
+		 for(int i=0;i<10;i++) total+=prices[i];
+		//  average of prices
+		 average=total/10;
 
-	cout << fixed << setprecision(2);
-	cout << "Average stock price: $" << average << endl;
-	return 0;
-}	//end of main function
+		 cout << fixed << setprecision(2);
+		 cout << "Average stock price: $" << average << endl;
+		 return 0;
 
+} //end of main function
